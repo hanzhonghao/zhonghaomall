b6c25b9d2c7de8d46ddaecb9b4ae0d9d2cdbda6b
hzh <hanzhonghao593513235@gmail.com>
2018-08-23 07:16:58 +0800

    code at home

---
+++ src/main/java/com/zhonghaomall/common/Test1.java
@@ -0,0 +1,12 @@
+package com.zhonghaomall.common;
+
+/**
+ * Created by 小豪 on 2018/8/23.
+ */
+public class Test1 {
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("sssss");
+        return super.equals(obj);
+    }
+}
