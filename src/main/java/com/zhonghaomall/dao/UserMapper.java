9af2b563607ba8f9fb07922a2b7581e98e942f01
zhonghao.han <zhonghao.han@nuance.com>
2018-08-22 18:16:44 +0800

    Second commit

---
+++ src/main/java/com/zhonghaomall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zhonghaomall.dao;
+
+import com.zhonghaomall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
